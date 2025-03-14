4baf3118b1a9b163a5984cbb025ca261551188d2
Femi422 <femibadejo2002@yahoo.com>
2025-03-14 22:22:35 +0000

    Update main.tf
    Changed availability zone to "az"

--- network/vpc/main.tf
+++ network/vpc/main.tf
@@ -23,7 +23,7 @@ module "dynamic_subnets" {
   namespace          = var.account_type
   stage              = var.env
   name               = var.name
-  availability_zones = ["eu-west-2a","eu-west-2b","eu-west-2c"]
+  az                 = ["eu-west-2a","eu-west-2b","eu-west-2c"]
   nat_gateway_enabled  = false
   vpc_id             = module.vpc.vpc_id
   igw_id             = [module.vpc.igw_id]
